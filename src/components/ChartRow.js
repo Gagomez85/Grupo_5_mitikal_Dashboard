6724dd6cf07ddfcb98fefa59efe38ce01d1b334c
Fernando Rodriguez <fernandorodriguez@smsconsulting.mobi>
2021-09-13 14:49:59 -0300

    dashboard mitikal 1309

--- src/components/ChartRow.js
+++ src/components/ChartRow.js
@@ -1,24 +1,15 @@
-import React from 'react';
+import React from "react";
 
-
-function ChartRow(props){
-    return (
-                <tr>
-                    <td>{props.Title}</td>
-                    <td>{props.Length}</td>
-                    <td>{props.Rating}</td>
-                    <td>
-                        <ul>
-                            {props.Categories.map( (category,i) => 
-                                <li key={`category ${i}`}>{category}</li>
-                            )}
-                        </ul>
-                    </td>
-                    <td>{props.Awards}</td>
-                </tr>
-            )
-    }
-    
-        
-
+function ChartRow(props) {
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.description}</td>
+      <td>{props.category}</td>
+      <td>{props.color}</td>
+      <td>{props.size}</td>
+      <td>{props.price}</td>
+    </tr>
+  );
+}
 export default ChartRow;
