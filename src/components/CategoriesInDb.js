99ef9bb22540d1238a811954e66a5c0e185c4f66
gagomez85 <gagomez85@gmail.com>
2021-09-17 15:29:17 -0300

    Sprint 8 - Dashboard - Categorias

--- src/components/CategoriesInDb.js
+++ src/components/CategoriesInDb.js
@@ -5,7 +5,7 @@ function CategoriesInDb({ description }) {
       <div className="card shadow mb-4">
         <div className="card-header py-3">
           <h5 className="m-0 font-weight-bold text-gray-800">
-            Modelos de Productos
+            Categorias
           </h5>
         </div>
         <div className="card-body">
