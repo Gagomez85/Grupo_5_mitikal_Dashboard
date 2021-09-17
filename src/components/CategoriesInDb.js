9acf350c3e259dfadc1080ff850072322ab3f15f
gagomez85 <gagomez85@gmail.com>
2021-09-17 15:28:29 -0300

    Dashboard - Categorias

---
+++ src/components/CategoriesInDb.js
@@ -0,0 +1,41 @@
+import React from "react";
+function CategoriesInDb({ description }) {
+  return (
+    <div className="col-lg-6 mb-4">
+      <div className="card shadow mb-4">
+        <div className="card-header py-3">
+          <h5 className="m-0 font-weight-bold text-gray-800">
+            Modelos de Productos
+          </h5>
+        </div>
+        <div className="card-body">
+          <div className="row">
+            {description && description.length > 0 ? (
+              description.map((item) => {
+                return (
+                  <div className="col-lg-6 mb-4">
+                    <div className="card bg-danger text-white shadow">
+                      <div className="card-body">
+                        <p>{item.name}</p>
+                        <p>{item.Total}</p>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <div className="col-lg-6 mb-4">
+                <div className="card bg-danger text-white shadow">
+                  <div className="card-body">
+                    <p>No Hay Categorias Disponibles</p>
+                  </div>
+                </div>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default CategoriesInDb;
--- src/components/MitikalProducts.js
+++ src/components/MitikalProducts.js
@@ -1,20 +1,20 @@
 import { useState, useEffect } from "react";
-import GenresInDb from "./GenresInDb";
+import CategoriesInDb from "./CategoriesInDb";
 
 function MitikalProducts() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch("http://127.0.0.1:3050/api/products/")
+    fetch("http://127.0.0.1:3050/api/category/listCategoryTotal")
       .then((response) => response.json())
       .then((data) => {
-        setProducts(data.data.products);
+        setProducts(data.data.category);
       })
       .catch((error) => console.error(error));
   }, []);
   console.log(products);
   return (
-    <>{products.length > 0 ? <GenresInDb description={products} /> : ""}</>
+    <>{products.length > 0 ? <CategoriesInDb description={products} /> : ""}</>
   );
 }
 
